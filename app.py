e46aeb604849b7facd1842e56dfd7b7498da4f4b
ankampriyanka <ankampriyanka.23@gmail.com>
2025-12-05 16:02:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello from GitLab → HuggingFace CI/CD!\n This is just a sample project to understand the deployement of app from Github to HuggingFace"}
+    return {"message": "Hello from GitLab → HuggingFace CI/CD!\n This is just a sample project to understand the deployement of app from Github to HuggingFace!!"}
 
 #if __name__ == "__main__":
    # uvicorn.run("app:app", host="0.0.0.0", port=8000) - Not needed for deploying in huggingface
